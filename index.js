4cb14ce65e473678c1ed580c36a3de4c8df77022
fatinaelomar3 <fatinaelomar3@gmail.com>
2023-05-29 19:43:25 +0300

    add cors

--- index.js
+++ index.js
@@ -23,12 +23,12 @@ app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 // app.use(cookieParser());
-// const corsOptions = {
-//   origin: "http://localhost:3000",
-//   credentials: true, //access-control-allow-credentials:true
-//   optionSuccessStatus: 200,
-// };
-// app.use(cors(corsOptions));
+const corsOptions = {
+  origin: "http://localhost:3000",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
 app.use("/dish",Product_routes);
 app.use("/category",CategoryRoutes);
 
